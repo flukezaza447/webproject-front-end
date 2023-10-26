3f99fa8ef343289108dabc63149e2e1f694de6a1
OLympuz <fluk1447@hotmail.com>
2023-10-26 23:34:46 +0700

    dsaa

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Router from './loutes/Router'
+
+function App() {
+  return (
+    <>
+      <Router />
+    </>
+  );
+}
+
+export default App;
